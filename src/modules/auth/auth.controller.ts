b940b302be1509666a9fd58f28a099c5a28692c9
Rafi <itzmeh320@gmail.com>
2025-10-03 14:44:37 +0600

    update

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -4,15 +4,14 @@ import { AuthService } from "./auth.service";
 const login = async (req: Request, res: Response) => {
   try {
     const result = await AuthService.login(req.body);
-    const isProd = process.env.NODE_ENV === "production";
     res
       .cookie("token", result.token, {
         httpOnly: true,
-        secure: isProd,
-        sameSite: isProd ? "none" : "lax",
-        maxAge: 24 * 60 * 60 * 1000,
+        secure: true,
+        sameSite:"none",
+
       })
-      .json({ user: result.user });
+      .json({ user: result.user, token:result.token });
   } catch (error: any) {
     res.status(401).json({ message: error?.message ?? "Unauthorized" });
   }
